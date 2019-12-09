de86076a932277c3988c338c95230ea9e06a8701
Hiquery <haihaidb@gmail.com>
2019-12-09 21:30:35 +0700

    Updates

---
+++ precache-manifest.a023ffef07610ddfd1a5929b90114478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b260fd286e8606d5421b60bcf043e6",
+    "url": "/Calculator-app-reactjs/index.html"
+  },
+  {
+    "revision": "3ed6983be67f3232ab81",
+    "url": "/Calculator-app-reactjs/static/css/main.69e1e884.chunk.css"
+  },
+  {
+    "revision": "25c21be8e66da0bff54e",
+    "url": "/Calculator-app-reactjs/static/js/2.e7814c3b.chunk.js"
+  },
+  {
+    "revision": "3ed6983be67f3232ab81",
+    "url": "/Calculator-app-reactjs/static/js/main.07251893.chunk.js"
+  },
+  {
+    "revision": "004439fec407da38fed7",
+    "url": "/Calculator-app-reactjs/static/js/runtime-main.9d0846d6.js"
+  },
+  {
+    "revision": "1ed5f18366c7798b1687ce9945d8fb86",
+    "url": "/Calculator-app-reactjs/static/media/calculator.1ed5f183.jpg"
+  }
+]);
